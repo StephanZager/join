7da02cc181cbf4ba540f090b040e34f1ab01ae6b
GuerdalM <g.moumin@web.de>
2024-05-09 11:17:46 +0200

    LoginPage Edit

--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('login-form');
+    const signupSection = document.querySelector('.signup-section');
+    const footer = document.querySelector('footer');
 
-document.addEventListener("DOMContentLoaded", function() {
-    const logo = document.getElementById('logo');
-    const loginForm = document.getElementById('login-form');
-    logo.addEventListener('animationend', function() {
-        loginForm.classList.add('show');
-    });
+    setTimeout(() => {
+        form.classList.add('show');
+        signupSection.classList.add('signup-show');
+        footer.classList.add('show');
+    }, 1600); 
 });
-
