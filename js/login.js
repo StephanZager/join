9a6fb96d16950c843bd887c8bd58830015041357
GuerdalM <g.moumin@web.de>
2024-05-11 13:09:43 +0200

    Sign Up Section

--- js/login.js
+++ js/login.js
@@ -2,10 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById('login-form');
     const signupSection = document.querySelector('.signup-section');
     const footer = document.querySelector('footer');
+    const backgroundOverlay = document.getElementById('background-overlay');
 
     setTimeout(() => {
         form.classList.add('show');
         signupSection.classList.add('signup-show');
         footer.classList.add('show');
-    }, 1600); 
+    }, 1200);
+
+    const logo = document.getElementById('logo');
+    logo.addEventListener('animationstart', function () {
+        backgroundOverlay.classList.add('hidden');
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const backgroundOverlay = document.getElementById('background-overlay');
+    const logo = document.getElementById('logo');
+
+    // Versteckt das Overlay, nachdem die Logoanimation abgeschlossen ist
+    logo.addEventListener('animationend', function () {
+        backgroundOverlay.classList.add('hidden');
+    });
 });
