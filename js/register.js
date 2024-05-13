11b102d9e2eed07313febdc3dd20ddf979274742
Johannes Linnecke <139555095+JLinnecke@users.noreply.github.com>
2024-05-13 20:37:56 +0200

    first code in register.js

--- js/register.js
+++ js/register.js
@@ -1,31 +1,37 @@
-let users = [];
-
-
-async function init(){
-    loadUsers();
+function submitData(event) {
+    event.preventDefault(); // Verhindert das Standardverhalten des Formulars
+    
+    let name = document.getElementById('userName').value;
+    let email = document.getElementById('userEmail').value;
+    let password = document.getElementById('userPassword').value;
+    
+    let userData = {
+        name: name,
+        email: email,
+        password: password
+    };
+    
+    // Daten an Firebase senden
+    postData(userData);
 }
 
-async function loadUsers(){
-    try {
-        users = JSON.parse(await getItem('users'));
-    } catch(e){
-        console.error('Loading error:', e);
-    }
-}
 
-
-async function register() {
-    registerBtn.disabled = true;
-    users.push({
-        email: email.value,
-        password: password.value,
+async function postData(userData) {
+    await fetch('https://join-ac3b9-default-rtdb.europe-west1.firebasedatabase.app/', {
+        method: 'POST',
+        body: JSON.stringify(userData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Data sent successfully:', data);
+        // Hier kannst du die Daten weiterverarbeiten, wenn nötig
+    })
+    .catch(error => {
+        console.error('Error sending data:', error);
     });
-    await setItem('users', JSON.stringify(users));
-    resetForm();
-}
-
-function resetForm() {
-    email.value = '';
-    password.value = '';
-    registerBtn.disabled = false;
 }
