792b41d10bd8585c335ce641f1b2447a9ad27898
StephanZager <Stephan-Zager@web.de>
2024-05-22 19:34:00 +0200

    function for numbers summary

--- js/summary.js
+++ js/summary.js
@@ -1,4 +1,4 @@
-let numberOfBoard =[];
+let numberOfBoard = [];
 
 
 /**
@@ -19,7 +19,7 @@ function greeting() {
     }
 }
 
-async function loadNumberOfBoard(path = "/userTask"){
+async function loadCategory(path = "/userTask") {
     try {
         let response = await fetch(BASE_URL + path + ".json");
         let responseToJson = await response.json();
@@ -29,8 +29,7 @@ async function loadNumberOfBoard(path = "/userTask"){
                 let number = responseToJson[key];
 
                 numberOfBoard.push({
-                    todos : number.category
-                   
+                    todos: number.category
                 });
             }
         }
@@ -40,12 +39,41 @@ async function loadNumberOfBoard(path = "/userTask"){
         return null;
     }
 }
-    
 
-    
+async function taskAssignment() {
+    let numberOfTodos = [];
+    let numberOfDone = [];  
+    let numberOfAwaitFeedback = [];
+    let numberOfInProgress = [];
+
+    for (let i = 0; i < numberOfBoard.length; i++) {
+        const task = numberOfBoard[i]['todos'];
+
+        if (task.indexOf('toDo') !== -1) {
+            numberOfTodos.push(task);
+        }else if(task.indexOf('done') !== -1){
+            numberOfDone.push(task);            
+        }else if(task.indexOf('awaitFeedback') !== -1){
+            numberOfAwaitFeedback.push(task); 
+        }else if(task.indexOf('inProgress') !== -1){
+            numberOfInProgress.push(task); 
+        }                 
+    } 
+    await renderSummary(numberOfTodos, numberOfDone, numberOfAwaitFeedback, numberOfInProgress); 
+    }
+
+async function renderSummary(numberOfTodos, numberOfDone, numberOfAwaitFeedback, numberOfInProgress) {
+    document.getElementById('howMuchTodos').innerHTML = numberOfTodos.length;
+    document.getElementById('howMuchDone').innerHTML = numberOfDone.length;
+    document.getElementById('howMuchAwaitFeedback').innerHTML = numberOfAwaitFeedback.length;
+    document.getElementById('howMuchInProgress').innerHTML = numberOfInProgress.length; 
+}  
 
 
-function initSummary() {
-    greeting();
-    loadNumberOfBoard();
-}
+
+
+    async function initSummary() {
+        greeting();
+        await loadCategory();
+        taskAssignment()
+    }
