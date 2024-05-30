11f0b4a68220c2fa5afffa3074543e8887fedb4d
Johannes Linnecke <139555095+JLinnecke@users.noreply.github.com>
2024-05-30 15:49:58 +0200

    add popup for edit task/make css changes for edit popup

--- js/edit_task.js
+++ js/edit_task.js
@@ -1,115 +1,81 @@
-let isEditing = false;
+function openEditTask() {
+    let editTaskPopup = document.createElement('div');
 
-function openTaskPopup() {
-    let modal = document.getElementById('addTaskModel');
-    let span = document.getElementsByClassName('close')[0];
-   
-    modal.style.display = 'block';
-    
-    span.onclick = function () {
-        modal.style.display = 'none';
-    }
-    
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    }
+    editTaskPopup.className = 'edit-task';
+    editTaskPopup.style.position = 'fixed';
+    editTaskPopup.style.top = '48%';
+    editTaskPopup.style.left = '50%';
+    editTaskPopup.style.transform = 'translate(-50%, -50%)';
+    editTaskPopup.style.backgroundColor = '#fff';
+    editTaskPopup.style.zIndex = '99';
+
+    editTaskPopup.innerHTML = generateEditTaskHTML();
+
+    document.body.appendChild(editTaskPopup);
 }
 
+function generateEditTaskHTML() {
+  return `
+    <main class="edit-task-menü" >
+            <div class="left-field-section">
+                <div class="addtaks-desktop">
+                    <span>Title<span style="color: red;">*</span></span>
+                    <input id="title" class="title-select-or-input" placeholder="Enter a title" type="text" required>
+                    <span class="required-info">This field is required</span>
+                </div>
 
-function removeOnsubmit() {
-    // Entfernen Sie das onsubmit-Event des Formulars
-    let form = document.getElementById('addTaskFormRemove');
-    if (form) {
-        form.onsubmit = null;
-    } else {
-        console.error("Formular mit ID 'addTaskFormRemove' nicht gefunden.");
-    }
+                <div class="addtaks-desktop">
+                    <span>Description</span>
+                    <textarea id="description" class="title-select-or-input" placeholder="Enter a Description" type="text"></textarea>
+                </div>
 
-    // Fügen Sie ein onclick-Event zum Bestätigungsbutton hinzu
-    let confirmBtn = document.getElementById('confirmBtn');
-    if (confirmBtn) {
-        confirmBtn.onclick = async function(event) {
-            event.preventDefault(); // Verhindert das Absenden des Formulars
-            await editTask();
-        };
-    } else {
-        console.error("Button mit ID 'confirmBtn' nicht gefunden.");
-    }
-}
+                <div class="addtaks-desktop dropdown">
+                    <button type="button" class="dropdown-button">Dropdown <img src="assets/img/arrow_drop_down.png"></button>
+                    <div class="dropdown-content" id="assigned">
+                        <!-- Die Optionen mit Checkboxen werden hier generiert -->
+                    </div>
+                </div>
+            </div>
+            <div class="right-field-section">
+                <div class="addtaks-desktop">
+                    <span>Due Date <span style="color: red;">*</span></span>
+                    <input id="dueDate" class="title-select-or-input" type="date" placeholder="dd/mm/yyyy" required>
+                    <span class="required-info">This field is required</span>
+                </div>
 
+                <div class="prio-section addtaks-desktop">
+                    <span class="subheadline"><b>Prio</b></span>
+                    <div class="prio-buttons">
+                        <button class="urgent-button" type="button" onclick="setPriority('Urgent')">Urgent<img class="prio-icons" src="./assets/img/prio-urgent-icon-unclicked.png"></button>
+                        <button class="medium-button" type="button" onclick="setPriority('Medium')">Medium<img class="prio-icons" src="./assets/img/prio-medium-icon-unclicked.png"></button>
+                        <button class="low-button" type="button" onclick="setPriority('Low')">Low<img class="prio-icons" src="./assets/img/prio-low-icon-unclicked.png"></button>
+                    </div>
+                </div>
 
-async function editTask(){
-    
-    openTaskPopup();
-    removeRequierdAttribute();
-    let confirmBtn = document.getElementById('confirmBtn');
+                <div class="addtaks-desktop">
+                    <span>Category<span style="color: red;">*</span></span>
+                    <select id="category" class="title-select-or-input" required>
+                        <option disabled selected hidden> Select task category</option>
+                        <option>Technical Task</option>
+                        <option>User Story</option>
+                    </select>
+                    <span class="required-info">This field is required</span>
+                </div>
 
-    confirmBtn.childNodes.forEach(node => {
-        if (node.nodeType === Node.TEXT_NODE) {
-            node.textContent = 'OK';
-        }
-    });
-
-    document.getElementById('test').classList.add('edit-task');
-    document.getElementById('test').classList.remove('modal-addtask-popup');
-    document.getElementById('maincontainerAddTask').classList.remove('maincontainer-desktop');
-    document.getElementById('maincontainerAddTask').classList.add('edit-task-menü');
-    document.getElementById('spanRequiredInfo').style.display = 'none';
-    document.getElementById('clearBtn').style.display = 'none';
-    document.getElementById('buttonLastSection').style.marginTop = '32px';
-    confirmBtn.innerHTML = 'OK <img src="assets/img/check-button-add-task.png" alt="Confirm">';
-
-}
-
-function removeRequierdAttribute () {
-    const idToRemoveRequired = ['title', 'dueDate', 'category'];
-
-    idToRemoveRequired.forEach(elementId => {
-        const element = document.getElementById(elementId);
-
-        if (element) {
-            element.removeAttribute('required');
-        } else {
-            console.error(`Element mit ID ${elementId} nicht gefunden.`)
-        }
-    });
-}
-
-function setupFormSubmit() {
-    let form = document.getElementById('addTaskFormRemove');
-    if (form) {
-        form.onsubmit = async function(event) {
-            event.preventDefault(); // Verhindert das Absenden des Formulars
-            if (isEditing) {
-                await editTask();
-            } else {
-                // Fügen Sie die Aufgabe hinzu...
-            }
-            isEditing = false;
-        };
-    } else {
-        console.error("Formular mit ID 'addTaskFormRemove' nicht gefunden.");
-    }
-}
-
-window.onload = function() {
-    setupFormSubmit();
-};
-
-async function postEditTask(taskData) {
-    let response = await fetch(BASE_URL + path + '/' + taskId + '.json', {
-        method: "PUT",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(taskData)
-    });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    return await response.json();
+                <div class="addtaks-desktop">
+                    <span>Subtasks</span>
+                    <div class="input-with-button">
+                        <input id="subtasks" class="title-select-or-input" placeholder="Add new Subtasks">
+                        <button type="button" onclick="addSubtaskToList()"><img class="input-button-img" src="assets/img/add.png" alt=""></button>
+                    </div>
+                    <ul id="subtaskList"></ul>
+                </div>
+            </div>
+        </main>
+        <div class="last-section">
+            <div class="button-last-section">
+                <button type="button" class="create-task-button">Ok <img src="./assets/img/check-button-add-task.png" alt=""></button>
+            </div>
+        </div>
+    `;
 }
